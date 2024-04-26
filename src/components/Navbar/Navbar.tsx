6eab3002df38c0beb0e37eb3010dc5649d1a0f71
NagaprasadVr <nagaprasadvr246@gmail.com>
2024-04-26 07:12:42 +0530

    fix margin

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   CircularProgress,
+  Divider,
   useMediaQuery,
   useTheme,
 } from "@mui/material";
@@ -35,114 +36,126 @@ export const Navbar = () => {
     <nav
       style={{
         display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "100%",
-        height: "50px",
+        flexDirection: "column",
         zIndex: 1000,
         position: "sticky",
         top: 0,
-        backgroundColor: "transparent",
-        backdropFilter: "blur(30px)",
-        border: "2px solid #36454F",
-        borderRadius: "10px",
-        paddingTop: "10px",
-        paddingBottom: "10px",
       }}
     >
       <Box
         sx={{
           display: "flex",
-          flexDirection: "row",
-          gap: "20px",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+          height: "50px",
+          backgroundColor: "transparent",
+          backdropFilter: "blur(30px)",
+          paddingTop: "10px",
+          paddingBottom: "10px",
         }}
       >
         <Box
-          display={"flex"}
-          flexDirection={"row"}
-          gap="10px"
-          ml="10px"
-          width={mobileScreen ? "10%" : "20%"}
-          minWidth={"250px"}
-          justifyContent={"flex-start"}
-          alignItems={"center"}
           sx={{
-            cursor: "pointer",
+            display: "flex",
+            flexDirection: "row",
+            gap: "20px",
           }}
         >
-          <Logo size={30} />
-        </Box>
+          <Box
+            display={"flex"}
+            flexDirection={"row"}
+            gap="10px"
+            ml="10px"
+            width={mobileScreen ? "10%" : "20%"}
+            minWidth={"250px"}
+            justifyContent={"flex-start"}
+            alignItems={"center"}
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <Logo size={30} />
+          </Box>
 
-        <Box
-          sx={{
-            display: mobileScreen ? "none" : "flex",
-            gap: "10px",
-            justifyContent: "center",
-            paddingRight: "20px",
-          }}
-        >
-          {NavLinks.map((nav) => (
-            <Link
-              href={nav.link}
-              key={nav.name}
-              style={{
-                width: "auto",
-              }}
-            >
-              <Button
-                sx={{
-                  color: nav.color,
-                  backgroundColor: "black",
-                  "&:hover": {
-                    color: "white",
-                    backgroundColor: "transparent",
-                  },
+          <Box
+            sx={{
+              display: mobileScreen ? "none" : "flex",
+              gap: "10px",
+              justifyContent: "center",
+              paddingRight: "20px",
+            }}
+          >
+            {NavLinks.map((nav) => (
+              <Link
+                href={nav.link}
+                key={nav.name}
+                style={{
+                  width: "auto",
                 }}
               >
-                {nav.name}
-              </Button>
-            </Link>
-          ))}
+                <Button
+                  sx={{
+                    color: nav.color,
+                    backgroundColor: "black",
+                    "&:hover": {
+                      color: "white",
+                      backgroundColor: "transparent",
+                    },
+                  }}
+                >
+                  {nav.name}
+                </Button>
+              </Link>
+            ))}
+          </Box>
         </Box>
-      </Box>
 
-      {isLoading ? (
-        <Box
-          sx={{
-            p: "10px",
-          }}
-        >
-          <CircularProgress
-            size={30}
-            thickness={6}
+        {isLoading ? (
+          <Box
             sx={{
-              color: "primary",
+              p: "10px",
             }}
-          />
-        </Box>
-      ) : mobileScreen ? (
-        <Box
-          sx={{
-            display: mobileScreen ? "flex" : "none",
-            gap: "10px",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "10px",
-          }}
-        >
-          <MobileNav />
-        </Box>
-      ) : (
-        <Box
-          display={mobileScreen ? "none" : "flex"}
-          sx={{
-            mr: "20px",
-            ml: "20px",
-          }}
-        >
-          <Wallet setOpen={null} />
-        </Box>
-      )}
+          >
+            <CircularProgress
+              size={30}
+              thickness={6}
+              sx={{
+                color: "primary",
+              }}
+            />
+          </Box>
+        ) : mobileScreen ? (
+          <Box
+            sx={{
+              display: mobileScreen ? "flex" : "none",
+              gap: "10px",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "10px",
+            }}
+          >
+            <MobileNav />
+          </Box>
+        ) : (
+          <Box
+            display={mobileScreen ? "none" : "flex"}
+            sx={{
+              mr: "20px",
+              ml: "20px",
+            }}
+          >
+            <Wallet setOpen={null} />
+          </Box>
+        )}
+      </Box>
+      <Divider
+        sx={{
+          borderBottomWidth: "3px",
+          // ml: "-10px",
+          // mr: "-10px",
+        }}
+      />
     </nav>
   );
 };
